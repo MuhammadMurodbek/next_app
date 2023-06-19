7450994c08ce08ffdc6e5d3e02eef26729f78441
Murodbek Raximjonov <murodbekraximjonov@Murodbeks-Air.home>
2023-06-19 11:30:02 +0500

    route_to_user

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
 // import Clients from "./clients/page";
+"use client";
+
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const router = useRouter();
+  router.push("/user");
   return <main>s</main>;
 }
